2e8a5c9422ad7c29f7fde57501aa7a0198a0eda1
Sam <samuelthammond@gmail.com>
2025-03-11 18:50:14 -0400

    Initial commit

---
+++ test/il6-compliant-aws-env.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Il6CompliantAwsEnv from '../lib/il6-compliant-aws-env-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/il6-compliant-aws-env-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Il6CompliantAwsEnv.Il6CompliantAwsEnvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
